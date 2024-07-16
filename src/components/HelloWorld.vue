39ae91f542b3c434f9db426e91d915a64c953c82
PassionDev206 <bluewolf98219@gmail.com>
2024-07-16 05:48:14 -0700

    Added the vuetify into the existing project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,17 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <v-app>
+    <v-main>
+      <v-container>Hello Vuetify</v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: 'App',
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+/* Add your styles here */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// main.js or main.ts
+import { createApp } from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify'; // path to vuetify export
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(vuetify);
+app.mount('#app');
