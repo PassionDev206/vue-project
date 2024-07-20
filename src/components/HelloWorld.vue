1d01487a2d8144c9c2a35961ee016fb4205e074d
PassionDev206 <bluewolf98219@gmail.com>
2024-07-19 18:14:15 -0700

    Updated to test the event and computed properties

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,22 @@
 <template>
-  <form class="container" v-on:submit.prevent="items.push({ id: items.length + 1, label: newItem })">
-    <div class="shoping-list-card">
-      <h1>{{ header }}</h1>
-      <input
-        type="text"
-        placeholder="Add an item"
-        v-model="newItem"
-      />
+  <div class="container">
+    <h1>{{ header }}</h1>
+    <button class="btn" v-if="editing" @click="doEdit(false)">Cancel</button>
+    <button class="btn btn-primary" v-else @click="doEdit(true)">Add Item</button>
+    <a v-bind:href="newItem">Dynamic Link</a>
+    <form class="shoping-list-card" v-if="editing" @submit.prevent="saveItem">
+      <input type="text" placeholder="Add an item" v-model="newItem" />
       <label>
         <input type="checkbox" v-model="newItemHighPriority" />
         High Priority
       </label>
-      <button
-        class="btn btn-primary"
-        type="submit"
-      >
-        Save Item
-      </button>
+      <button class="btn btn-primary" type="submit">Save Item</button>
       <ul>
         <li v-for="{ id, label } in items" :key="id">{{ label }}</li>
       </ul>
-    </div>
-  </form>
+      <p v-if="!items.length">Nothing to see here</p>
+    </form>
+  </div>
 </template>
 
 <script setup>
@@ -29,22 +24,32 @@ import { ref } from "vue";
 
 const header = ref("Shopping List App");
 const items = ref([
-  { id: 1, label: "10 party hats" },
-  { id: 2, label: "2 board games" },
-  { id: 3, label: "20 cups" },
+  // { id: 1, label: "10 party hats" },
+  // { id: 2, label: "2 board games" },
+  // { id: 3, label: "20 cups" },
 ]);
 const newItem = ref("");
 const newItemHighPriority = ref(false);
+const editing = ref(false)
+const saveItem = () => {
+  items.value.push({ id: items.value.length + 1, label: newItem.value });
+  newItem.value = "";
+};
+
+const doEdit = (e) => {
+  editing.value = e
+  newItem.value = ""
+}
 </script>
 
 <style>
 .container {
   display: flex;
   justify-content: center;
+  background-color: darkgray;
+  padding: 50px 30px;
 }
 .shoping-list-card {
-  padding: 50px 30px;
-  background-color: darkgray;
   max-width: 500px;
 }
 .shoping-list-card ul {
